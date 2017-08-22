e1261f2a9a873548621d9de267ce1483db821152
Raghunath Dayala <rdayala@oradev.oraclecorp.com>
2017-08-22 20:35:54 +0530

    spring basic example

---
+++ springcore/springboot-starter-project/src/main/java/org/rdayala/spring/basics/springbootstarterproject/SpringbootStarterProjectApplication.java
@@ -0,0 +1,12 @@
+package org.rdayala.spring.basics.springbootstarterproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterProjectApplication.class, args);
+	}
+}
